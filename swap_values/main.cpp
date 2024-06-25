d845d9769851f75f82607a0080783198db5a9f41
Peng Du <rick.peng.du@gmail.com>
2024-06-25 13:11:37 -0700

    finish a swap file

--- swap_values/main.cpp
+++ swap_values/main.cpp
@@ -3,11 +3,11 @@
 int main() {
     int x = 5, y = 10;
     std::cout << "Before swap: x = " << x << ", y = " << y << "\n";
-    swap(x, y); // TODO: Which swap function is being called?
+    swap(x, y); // calling the swap taking reference as parameter
     std::cout << "After swap: x = " << x << ", y = " << y << "\n";
 
     float a = 1.5, b = 2.5;
     std::cout << "Before swap: a = " << a << ", b = " << b << "\n";
-    swap(&a, &b); // TODO: Which swap function is being called?
+    swap(&a, &b);
     std::cout << "After swap: a = " << a << ", b = " << b << "\n";
 }
--- swap_values/util.hpp
+++ swap_values/util.hpp
@@ -5,8 +5,7 @@ void swap(int &a, int &b);
 
 template<typename T>
 void swap(T *a, T *b) {
-    /*
-     * TODO: Swap the values pointed by the two int pointers a and b.
-     *  Better yet, can you do this without a third variable?
-     */
+    T temp = *a;
+    *a = *b;
+    *b = temp;
 }
